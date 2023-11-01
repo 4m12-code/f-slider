f5be284e5e513949aa682628030dace468067404
4m12-code-code <antmar@outlook.es>
2023-11-01 16:32:25 -0500

    primeros pasos

---
+++ fs modificado/js/actions.js
@@ -0,0 +1,24 @@
+jQuery(function($) {
+  // El slider que se sincroniza debe inicializarse primero
+  $('#carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,//Mueve solo el slide
+    itemWidth: 210,
+    itemMargin: 5,
+    asNavFor: '#slider',
+    mousedrag: true
+  });
+
+  $('#slider').flexslider({
+    animation: "slide",
+    controlNav: true, // Flechitas laterales
+    animationLoop: false, // Quitar el límite
+    slideshow: false,//Mueve solo el slide
+    direction: "horizontal",
+    mousedrag: true,  // Habilitar el arrastre
+  });
+
+  $('.holamundo').append('<h3>Hola mundo cruel</h3>')
+});
---
+++ fs modificado/resources/README.txt
@@ -0,0 +1,16 @@
+/*
+* jQuery FlexSlider v2.7.2
+* Copyright 2012 WooThemes
+* Contributing Author: Tyler Smith
+
+*Modificado para agragar la funcionalidad de interactuar con el slider meidante clic y arrastrar con el mouse.
+
+*funcionalidad agregada: mousedrag.
+    el simbolo asterisco (*) es Modificadoes.
+    el simbolo más (+) es Agregado.
+    el simbolo menos (-) es Quitado.
+*/
+
+archivos modificadoes
+jQuery.flexslider.js
+flexslider.css ".flexslider (*width y +overflow)"
